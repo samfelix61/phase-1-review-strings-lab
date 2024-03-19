45fcdceec671811eac731da428e06546efe2326c
samfelix61 <samfelix61@gmail.com>
2024-03-19 20:16:41 +0300

    review string lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = 'samfelix';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = '';
+
+// const shortGreeting = 'Welcome, ';
+// const shortGreeting = `Welcome, ${currentUser}`;
+// const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
